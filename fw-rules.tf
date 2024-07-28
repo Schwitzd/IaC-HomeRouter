a7022f5c544d912234aa4225746b2204603cfa69
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2024-07-28 18:48:46 +0200

    refactored fw-rules code

--- fw-rules.tf
+++ fw-rules.tf
@@ -1,3 +1,77 @@
+locals {
+  firewall_rules = {
+    role0 = {
+      action           = "accept"
+      chain            = "input"
+      comment          = "defconf: accept established,related,untracked"
+      connection_state = "established,related,untracked"
+    }
+    role1 = {
+      action           = "drop"
+      chain            = "input"
+      comment          = "defconf: drop invalid"
+      connection_state = "invalid"
+    }
+    role2 = {
+      action   = "accept"
+      chain    = "input"
+      comment  = "defconf: accept ICMP"
+      protocol = "icmp"
+    }
+    role3 = {
+      action      = "accept"
+      chain       = "input"
+      comment     = "defconf: accept to local loopback (for CAPsMAN)"
+      dst_address = "127.0.0.1"
+    }
+    role4 = {
+      action             = "drop"
+      chain              = "input"
+      comment            = "defconf: drop all not coming from LAN"
+      in_interface_list  = "!LAN"
+    }
+    role5 = {
+      action       = "accept"
+      chain        = "forward"
+      comment      = "defconf: accept in ipsec policy"
+      ipsec_policy = "in,ipsec"
+    }
+    role6 = {
+      action       = "accept"
+      chain        = "forward"
+      comment      = "defconf: accept out ipsec policy"
+      ipsec_policy = "out,ipsec"
+    }
+    role7 = {
+      action           = "fasttrack-connection"
+      chain            = "forward"
+      comment          = "defconf: fasttrack"
+      connection_state = "established,related"
+      hw_offload       = true
+    }
+    role8 = {
+      action           = "accept"
+      chain            = "forward"
+      comment          = "defconf: accept established,related,untracked"
+      connection_state = "established,related,untracked"
+    }
+    role9 = {
+      action           = "drop"
+      chain            = "forward"
+      comment          = "defconf: drop invalid"
+      connection_state = "invalid"
+    }
+    role10 = {
+      action               = "drop"
+      chain                = "forward"
+      comment              = "defconf: drop all from WAN not DSTNATed"
+      connection_nat_state = "!dstnat"
+      connection_state     = "new"
+      in_interface_list    = "WAN"
+    }
+  }
+}
+
 ## Firewall NAT
 resource "routeros_ip_firewall_nat" "role1" {
   action                    = "masquerade"
@@ -8,6 +82,22 @@ resource "routeros_ip_firewall_nat" "role1" {
 }
 
 ## Firewall Rules
+resource "routeros_ip_firewall_filter" "firewall_rules" {
+  for_each = local.firewall_rules
+
+  action               = each.value.action
+  chain                = each.value.chain
+  comment              = each.value.comment
+  connection_state     = lookup(each.value, "connection_state", null)
+  protocol             = lookup(each.value, "protocol", null)
+  dst_address          = lookup(each.value, "dst_address", null)
+  in_interface_list    = lookup(each.value, "in_interface_list", null)
+  ipsec_policy         = lookup(each.value, "ipsec_policy", null)
+  hw_offload           = lookup(each.value, "hw_offload", null)
+  connection_nat_state = lookup(each.value, "connection_nat_state", null)
+}
+
+/*
 resource "routeros_ip_firewall_filter" "role0" {
   action           = "accept"
   chain            = "input"
@@ -87,3 +177,4 @@ resource "routeros_ip_firewall_filter" "role10" {
   connection_state     = "new"
   in_interface_list    = "WAN"
 }
+*/
