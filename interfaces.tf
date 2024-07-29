952ca138ea945cb7b7c0c88e21a6ea2247749ff4
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2024-07-29 21:30:11 +0200

    interface list are managed in terraform

--- interfaces.tf
+++ interfaces.tf
@@ -1,16 +1,29 @@
 ## Interfaces - Locals
 locals {
   lan_interface_list = ["bridge", "vlan-myhome", "vlan-myiot"]
+  interface_lists = {
+    lan = {
+      comment = "LAN interfaces"
+      name    = "LAN"
+    }
+    wan = {
+      comment = "WAN interface"
+      name    = "WAN"
+    }
+  }
+
 }
 
 # Interfaces
-resource "routeros_interface_list" "lan" {
-  comment = "defconf"
-  name    = "LAN"
+resource "routeros_interface_list" "interfaces" {
+  for_each = local.interface_lists
+  
+  comment = each.value.comment
+  name    = each.value.name
 }
 
 resource "routeros_interface_list_member" "lan-list_member" {
   for_each  = toset(local.lan_interface_list)
   interface = each.value
-  list      = routeros_interface_list.lan.name
+  list      = local.interface_lists.lan.name
 }
