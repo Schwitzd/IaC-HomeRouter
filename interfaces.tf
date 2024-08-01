af3ea8a108ad3ac65814ab44a3798f2a279d340c
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2024-08-01 08:16:05 +0200

    added list to include all vlans, needed for fw rules

--- interfaces.tf
+++ interfaces.tf
@@ -1,23 +1,27 @@
 ## Interfaces - Locals
 locals {
-  lan_interface_list = ["bridge", "vlan-myhome", "vlan-myiot"]
+  lan_interface_list   = ["bridge", "vlan-myhome", "vlan-myiot"]
+  vlans_interface_list = ["vlan-myhome", "vlan-myiot"]
   interface_lists = {
     lan = {
       comment = "LAN interfaces"
       name    = "LAN"
+    }
+    vlans = {
+      comment = "List of all VLANs"
+      name    = "VLANs"
     }
     wan = {
       comment = "WAN interface"
       name    = "WAN"
     }
   }
-
 }
 
 # Interfaces
 resource "routeros_interface_list" "interfaces" {
   for_each = local.interface_lists
-  
+
   comment = each.value.comment
   name    = each.value.name
 }
@@ -26,4 +30,10 @@ resource "routeros_interface_list_member" "lan-list_member" {
   for_each  = toset(local.lan_interface_list)
   interface = each.value
   list      = local.interface_lists.lan.name
+}
+
+resource "routeros_interface_list_member" "vlans-list_member" {
+  for_each  = toset(local.vlans_interface_list)
+  interface = each.value
+  list      = local.interface_lists.vlans.name
 }
