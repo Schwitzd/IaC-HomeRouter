30747966b27dc46ecc46f8139b913abafebefb88
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2024-08-04 19:32:47 +0200

    enabled again lte

--- lte.tf
+++ lte.tf
@@ -1,14 +1,11 @@
-/*
 resource "routeros_interface_lte" "lte" {
   allow_roaming = false
   apn_profiles  = "default"
   band          = []
-  default_name  = "lte1"
   disabled      = false
   mtu           = "1500"
   name          = "lte1"
-  network_mode  = ["3g", "lte"]
-  sms_protocol  = null
-# sms_read
+  #  network_mode  = ["3g", "lte"]
+  sms_protocol = null
+  # sms_read
 }
-*/
